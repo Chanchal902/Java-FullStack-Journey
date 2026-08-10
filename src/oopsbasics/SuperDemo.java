56ddf4db28ec28ffc16cae5a514ed0d7015a1770
Chanchal902 <c.rani.engg@gmail.com>
2026-08-11 00:36:26 +0530

    feat: add super keyword demonstration code

--- src/oopsbasics/SuperDemo.java
+++ src/oopsbasics/SuperDemo.java
@@ -1,26 +1,32 @@
-class A{
-    A(){
+package oopsbasics;
+
+class A {
+    A() {
         System.out.println("---IN BASE CLASS DEFAULT CONSTRUCTOR---");
     }
-    A(int x){
-        System.out.println("---In parameterised constructor A---" + x);
+
+    A(int x) {
+        System.out.println("---In parameterised constructor oopsbasics.A---" + x);
     }
 }
-class B extends A{
-    B(){
+
+class B extends A {
+    B() {
         super(1000);    // To call the constructor with parameters , we use super();
         System.out.println("---IN DERIVED CLASS DEFAULT CONSTRUCTOR---");
     }
-    B(int y){
+
+    B(int y) {
         //super(1000);
-        System.out.println("---In parameterised constructor B---" + y);
+        System.out.println("---In parameterised constructor oopsbasics.B---" + y);
     }
 }
-public class SuperDemo {
-    public static void main(String[] args){
-        //when a derived class is extended from the base class , constructor of the base class is executed first followed by the constructor of the derived class..
-        B obj = new B();  // No-Argument constructor of B is called
 
-        B obj1=new B(999);
+public class SuperDemo {
+    public static void main(String[] args) {
+        //when a derived class is extended from the base class , constructor of the base class is executed first followed by the constructor of the derived class..
+        B obj = new B();  // No-Argument constructor of oopsbasics.B is called
+
+        B obj1 = new B(999);
     }
 }
