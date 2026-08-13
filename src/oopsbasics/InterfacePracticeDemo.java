a7fb01aae540100eab69e1f42b8123560ebc6b59
Chanchal902 <c.rani.engg@gmail.com>
2026-08-14 01:21:14 +0530

    style: format code and fix indentation

--- src/oopsbasics/InterfacePracticeDemo.java
+++ src/oopsbasics/InterfacePracticeDemo.java
@@ -1,37 +1,43 @@
 package oopsbasics;
 
-interface Paytm{
+interface Paytm {
     void checkBalance(double x);  // abstract method
-    void debited(double amount,double a);  //abstract method
+
+    void debited(double amount, double a);  //abstract method
 }
-interface CreditCard{
+
+interface CreditCard {
     void payBill(double y);  //abstract method
 }
 
-class Payment implements Paytm , CreditCard{
+class Payment implements Paytm, CreditCard {
     @Override
-    public void checkBalance(double x){
+    public void checkBalance(double x) {
         System.out.println("Total Balance: " + x);
     }
+
     @Override
-    public void debited(double amount, double a){
-        System.out.println("Current amount: " + (amount-a));
+    public void debited(double amount, double a) {
+        System.out.println("Current amount: " + (amount - a));
     }
+
     @Override
-    public void payBill(double y){
+    public void payBill(double y) {
         System.out.println("Bill paid: " + y);
     }
 }
-interface Check extends Paytm , CreditCard{
+
+interface Check extends Paytm, CreditCard {
 
 }
-public class InterfacePracticeDemo {
-    public static void main(String[] args){
-        Paytm obj=new Payment();
-        obj.checkBalance(2300);
-        obj.debited(40000,250);
 
-        CreditCard obj1=new Payment();
+public class InterfacePracticeDemo {
+    public static void main(String[] args) {
+        Paytm obj = new Payment();
+        obj.checkBalance(2300);
+        obj.debited(40000, 250);
+
+        CreditCard obj1 = new Payment();
         obj1.payBill(15000);
     }
 }
