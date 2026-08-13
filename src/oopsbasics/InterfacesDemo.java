caaf4ad2de27b4ecf0eb5aaa7e285f7f9b81b4ba
Chanchal902 <c.rani.engg@gmail.com>
2026-08-14 01:21:23 +0530

    style: format code and fix indentation

--- src/oopsbasics/InterfacesDemo.java
+++ src/oopsbasics/InterfacesDemo.java
@@ -1,38 +1,45 @@
 package oopsbasics;
 
-interface Vehicle{
+interface Vehicle {
     void speed(); //interface can have only abstract methods
+
     void color();
 }
+
 // can use multiple interface
-interface Building{
-     void house(int x);
+interface Building {
+    void house(int x);
 }
-class Around implements Vehicle,Building{
+
+class Around implements Vehicle, Building {
     @Override
-    public void speed(){
+    public void speed() {
         System.out.println("Speed of the vehicle is good!");
     }
+
     @Override
-    public void color(){
+    public void color() {
         System.out.println("color of the vehicle is blue!");
     }
+
     @Override
-    public void house(int x){
+    public void house(int x) {
         System.out.println("The house costs " + x + " to me!");
     }
 }
+
 //Inheritance in interface
-interface Environment extends Vehicle , Building{
+interface Environment extends Vehicle, Building {
 
 }
+
 public class InterfacesDemo {
-    public static void main(String[] args){
-        Vehicle obj=new Around();
+    public static void main(String[] args) {
+        Vehicle obj = new Around();
         obj.speed();
         obj.color();
 
-        Building obj1=new Around();
+        Building obj1 = new Around();
         obj1.house(50000);
     }
 }
