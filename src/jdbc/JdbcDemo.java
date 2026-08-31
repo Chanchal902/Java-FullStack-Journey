6e3d925a87bce31b10133f4f6a05258208a7df48
Chanchal902 <c.rani.engg@gmail.com>
2026-09-01 03:48:45 +0530

    Feat: Add JDBC connection setup in JdbcDemo

--- src/jdbc/JdbcDemo.java
+++ src/jdbc/JdbcDemo.java
@@ -1,4 +1,20 @@
 package jdbc;
 
+import java.sql.*;   //import JDBC classes
+
 public class JdbcDemo {
+    public static void main(String[] args) {
+        String url = "jdbc:postgresql://localhost:5432/dbs";
+        String username = "postgres";
+        String password = System.getenv("DB_PASSWORD");
+        try {
+            // establishing Connection
+            Connection con = DriverManager.getConnection(url, username, password);
+            System.out.println("Congratulations! Connection established successfully!");
+            con.close();
+        } catch (SQLException e) {
+            System.out.println("Connection Failed!");
+            e.printStackTrace();
+        }
+    }
 }
