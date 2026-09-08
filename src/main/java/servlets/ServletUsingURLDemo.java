d92ba3f77c0a1e6ca03d60277d1bb52c1d50a41d
Chanchal902 <c.rani.engg@gmail.com>
2026-09-09 03:21:17 +0530

    feat: add ServletUsingURLDemo to handle GET requests

---
+++ src/main/java/servlets/ServletUsingURLDemo.java
@@ -0,0 +1,25 @@
+package servlets;
+// Import required java libraries
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+// Extend HttpServlet class
+@WebServlet("/ServletUsingURLDemo")
+public class ServletUsingURLDemo extends HttpServlet{
+    public void doGet(HttpServletRequest request ,  HttpServletResponse response) throws ServletException , IOException{
+        // set the response type
+        response.setContentType("text/html");
+        // get the stream to write the data
+        PrintWriter pw = response.getWriter();
+
+        //write html in the stream
+        pw.println("<html> <body>");
+        pw.println("Hello! Welcome to servlet ");
+        pw.println("</body></html>");
+
+        //close the stream
+        pw.close();
+    }
+}
